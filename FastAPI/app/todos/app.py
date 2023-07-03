444afc917283d0fbcc7aaf4c39a64b81c51ec2dc
josemanuelgonzalezfornell <josemanuelgonzalezfornell@gmail.com>
2023-07-03 18:19:37 +0200

    Se ha impartido la clase de FastApi

--- FastAPI/app/todos/app.py
+++ FastAPI/app/todos/app.py
@@ -4,7 +4,7 @@ from todo import todo_router
 app = FastAPI()
 
 @app.get("/")
-async def welcome() -> dict:
+async def welcome() -> dict: #Assync sirve para que si no la llame no gaste memoria
     return {
     "message": "Hello World"
     }
